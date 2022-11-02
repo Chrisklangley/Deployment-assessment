35268d75d853dba7746605739dcefcd032893ce4
Christopher langley <christopherlangley@ChristophersMBP.lan>
2022-11-02 14:00:28 -0700

    added Procfile

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 
  app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'./client/index.html'))
+    res.sendFile(path.join(__dirname,'../client/index.html'))
 
  })
 
