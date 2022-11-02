8ad76d1856cabd9cde6a702a63e24fd6ad6c229c
Christopher langley <christopherlangley@ChristophersMBP.lan>
2022-11-02 13:09:45 -0700

    ready for deployment

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+ const app = express()
+
+ app.use(express.json())
+ app.use(cors())
+
+
+ app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '..client/index.html'))
+
+ })
+
+ const {PORT} =process.env
+ const port = PORT || 4006
+
+ app.listen(port, () => console.log(`Server is running on PORT ${port}`))
+
+
+
