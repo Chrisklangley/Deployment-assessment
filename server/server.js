0f99ef756147459c96d67759133f0a65c7487896
Christopher langley <christopherlangley@ChristophersMBP.lan>
2022-11-02 13:28:20 -0700

    removed IMG

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ const cors = require('cors')
 const path = require('path')
 
  const app = express()
-
  app.use(express.json())
  app.use(cors())
 
 
  app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.sendFile(path.join(__dirname,'../client/index.html'))
 
  })
 
